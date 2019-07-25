6a93ebefa0f192c41a702d829b9e073dac31d274
Mika Mäki <agentti002x@gmail.com>
2019-07-25 15:03:16 +0300

    Add unit tests for stage and camera modules

---
+++ tests/test_ni_camera.py
@@ -0,0 +1,31 @@
+import os
+import unittest
+
+from devices import ni_camera
+
+CAMERA_NAME = "cam0"
+QUALITY = 10000
+
+
+class NICameraTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.camera = ni_camera.NI_Camera(CAMERA_NAME, QUALITY)
+
+    def test_set_setting(self):
+        self.camera.set_cam_setting(ni_camera.CameraSettings.SHUTTER, 500)
+
+    def test_take_pic(self):
+        self.camera.take_pic()
+
+    def test_save_pic(self):
+        path = os.path.dirname(os.path.abspath(__file__))
+        filename = "test"
+        if os.path.exists(filename):
+            raise RuntimeError("Test image already exists")
+        self.camera.save_pic(directory=path, filename=filename)
+        file_path = "{}.png".format(os.path.join(path, filename))
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+        else:
+            raise AssertionError("Image was not created at {}".format(file_path))
---
+++ tests/test_simplemotion.py
@@ -0,0 +1,65 @@
+import unittest
+
+from devices import simplemotion as sm
+
+AXES = ["TTL232R", "TTL232R2", "TTL232R3"]
+
+
+class SimpleMotionTest(unittest.TestCase):
+    @staticmethod
+    def test_clear_faults():
+        for axis in AXES:
+            sm.clear_faults(axis)
+
+    @staticmethod
+    def test_communication_all():
+        sm.test_multi(AXES)
+
+    @staticmethod
+    def test_communication_single():
+        for axis in AXES:
+            sm.test(axis)
+
+    @staticmethod
+    def test_get_param():
+        for axis in AXES:
+            for param in sm.SmParam:
+                sm.get_param(axis, param)
+
+    @staticmethod
+    def test_homing():
+        for axis in AXES:
+            sm.homing(axis)
+        for axis in AXES:
+            sm.homing(axis, abort=True)
+
+    @staticmethod
+    def test_move_inc():
+        for axis in AXES:
+            sm.move_inc(axis, 100)
+
+    @staticmethod
+    def test_move_abs():
+        for axis in AXES:
+            sm.move_abs(axis, 0)
+
+    @staticmethod
+    def test_set_control_mode():
+        for axis in AXES:
+            sm.set_control_mode(axis, sm.ControlMode.VELOCITY)
+            sm.set_control_mode(axis, sm.ControlMode.TORQUE)
+            sm.set_control_mode(axis, sm.ControlMode.POSITION)
+
+    @staticmethod
+    def test_set_velocity_limit():
+        for axis in AXES:
+            sm.set_velocity_limit(axis, 8000)
+
+    @staticmethod
+    def test_set_accel_limit():
+        for axis in AXES:
+            sm.set_accel_limit(axis, 40)
+
+    @staticmethod
+    def test_version():
+        sm.version()
