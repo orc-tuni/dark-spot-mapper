5752626725ef0206e9744c33fb5079ff8b3ce693
Mika Mäki <agentti002x@gmail.com>
2019-10-01 15:01:32 +0300

    Add a note about OpenCV DLL error

--- devices/opencv_camera.py
+++ devices/opencv_camera.py
@@ -1,3 +1,8 @@
+# If you get the following error on Windows
+# "ImportError: DLL load failed: The specified module could not be found."
+# Please install Visual C++ redistributable from
+# https://support.microsoft.com/en-us/help/2977003/the-latest-supported-visual-c-downloads
+
 import cv2
 
 print(cv2.getBuildInformation())
