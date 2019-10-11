93ec976c6398df2bc4a05809dd82aa071e2b7e8c
Mika Mäki <mika.maki@tuni.fi>
2019-10-11 17:28:20 +0300

    Fix launching the mounting script from a desktop shortcut

--- mount_tuni.py
+++ mount_tuni.py
@@ -8,7 +8,22 @@ import subprocess
 MOUNTPOINT = "/media/lab/tuni"
 
 
+def yesno() -> bool:
+    res = input()
+    while res not in ["y", "n"]:
+        print("Please input y or n")
+        res = input()
+
+    if res == "y":
+        return True
+    elif res == "n":
+        return False
+    else:
+        raise RuntimeError()
+
+
 def mount():
+    print("Please input credentials for mounting the network drive")
     print("Username: ", end="")
     username = input()
 
@@ -17,10 +32,13 @@ def mount():
         stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE
     )
     proc.communicate()
+    print("The network drive should now be mounted")
 
 
 def unmount():
+    print("Unmounting")
     subprocess.run(["fusermount", "-u", MOUNTPOINT])
+    print("Unmounted")
 
 
 def main():
@@ -31,22 +49,23 @@ def main():
         unmount()
         return
 
-    if os.listdir(MOUNTPOINT):
+    if contents:
         print("Network drive appears to be already mounted. Unmount? (y/n)")
-        res = input()
-        while res not in ["y", "n"]:
-            print("Please input y or n")
-            res = input()
-
-        if res == "y":
+        res = yesno()
+        if res:
             unmount()
-        else:
-            return
-    else:
-        mount()
+        return
+
+    mount()
+    res = ""
+    while res != "u":
+        print("Enter u to unmount the drive")
+        res = input()
+    unmount()
 
     print("Ready. Press enter to exit")
     input()
+
 
 if __name__ == "__main__":
     main()
